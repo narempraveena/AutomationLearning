d12a8df3a6ec30ef29b8fca566aa096fa3905f37
narempraveena <narempraveena@gmail.com>
2025-06-27 15:11:01 +0530

    added new class

---
+++ src/main/java/pack1/TestingForGit.java
@@ -0,0 +1,10 @@
+package pack1;
+
+public class TestingForGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Testing for Github push");
+	}
+
+}
